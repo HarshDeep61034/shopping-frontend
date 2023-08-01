004ec70711fb61a7d1026c90288ecace33a92ef5
Harsh Deep <hdeep61034@gmail.com>
2023-08-01 07:13:16 +0530

    main section done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import './styles/App.css'
 import Navbar from './components/Navbar'
+import svg from './assets/addtocart.svg'
 function App() {
 
   return (
     <>
     <div className="app">
     <Navbar />
+    <div className="main">
+    <div className="para">
+    Welcome to <span className="bold">Bizzare Bazzar</span>, your one-stop online shop for all things weird and wonderful! Unleash your inner curiosity and discover the extraordinary in our diverse and delightfully unconventional marketplace.
+    </div>
+    <div className="addtocartimg">
+      <img className='cart-illustration' src={svg} alt="Illustration" />
+    </div>
+    </div>
+    <div className="moto">
+      <div>Elevate Your</div>
+      <div>Shopping: Unleash<span className='stroke'> Possibilities!</span></div>
+    </div>
     </div>
     </>
   )
